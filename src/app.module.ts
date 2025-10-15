38d42d2e5b358da1833adbf8ca84d6eec0ad659a
SamerAhmadDev <samer.ahmad.dev@gmail.com>
2025-10-15 10:53:01 +0400

    add health and root endpoints

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { SupabaseModule } from './supabase/supabase.module';
 import { UsersModule } from './users/users.module';
+import { HealthModule } from './health/health.module';
 
 @Module({
-  imports: [SupabaseModule, UsersModule],
+  imports: [SupabaseModule, UsersModule, HealthModule],
   controllers: [AppController],
   providers: [AppService],
 })
