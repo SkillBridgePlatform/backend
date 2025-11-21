905073a476cd8dbb1159a2fb7eb26a2cac3521ca
SamerAhmadDev <samer.ahmad.dev@gmail.com>
2025-11-21 13:33:48 +0400

    update wistia upload mechanism

--- src/file-upload/file-upload.module.ts
+++ src/file-upload/file-upload.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
+import { CoursesModule } from 'src/courses/courses.module';
 import { SupabaseModule } from 'src/supabase/supabase.module';
 import { FileUploadController } from './file-upload.controller';
 import { FileUploadService } from './file-upload.service';
 import { WistiaFileUploadService } from './wisitia-file-upload.service';
 
 @Module({
-  imports: [SupabaseModule],
+  imports: [SupabaseModule, CoursesModule],
   controllers: [FileUploadController],
   providers: [FileUploadService, WistiaFileUploadService],
   exports: [FileUploadService, WistiaFileUploadService],
